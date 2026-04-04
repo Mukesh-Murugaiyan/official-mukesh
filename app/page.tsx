f9b96e5e4ac1714b04baeecf3c61ecc57c2f2d04
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2026-04-04 15:43:42 +0530

    MS: performance and seo fixed

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <h1 className="sr-only">
-        Mukesh Murugaiyan (themukesh) – Full Stack Developer & Software Engineer
-      </h1>
-
-      <p className="sr-only">
-        Mukesh Murugaiyan, professionally known as themukesh, is a leading Full
-        Stack Software Developer from India. He specializes in building scalable,
-        high-performance web and mobile applications using React, Next.js, and
-        React Native. Explore his portfolio at themukesh.com.
-      </p>
-
       <HomePageClient />
     </>
   );
