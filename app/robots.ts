096b7aae1036258f07fb28e129230541b57f8926
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2026-04-08 09:40:46 +0530

    nw commit

--- app/robots.ts
+++ app/robots.ts
@@ -12,8 +12,6 @@ export default function robots(): MetadataRoute.Robots {
           "/dashboard",
           "/instagram",
           "/api",
-          "/_next",
-          "/static",
         ],
       },
     ],
